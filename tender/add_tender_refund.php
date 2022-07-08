b8216d6d5aea7031c043ef91d408f14f06e38026
Chetan Chinchulkar <76626599+Chetan-Chinchulkar@users.noreply.github.com>
2022-07-08 20:33:47 +0530

    refund system

---
+++ tender/add_tender_refund.php
@@ -0,0 +1,59 @@
+<!-- refund status -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Add Tender Refund</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/price-range.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+<body>
+    <!-- form dropdown for refund status -->
+    <form action="add_tender_refund.php" method="post">
+        <select name="refund_status">
+            <option value="1">Refunded</option>
+            <option value="2">Not Refunded</option>
+        </select>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+
+
+<?php
+    // Path to the database file
+    require_once('connect.php');
+    // Check if the form has been submitted
+    if (isset($_POST['refund_status'])) {
+        // Get the form data
+        $refund_status = $_POST['refund_status'];
+        // Update the database
+        $sql = "UPDATE tender SET refund_status = '$refund_status' WHERE tender_id = '$tender_id'";
+        $result = mysqli_query($db, $sql);
+        // Check if the query was successful
+        if ($result) {
+            // Query was successful
+            echo '<p>Refund status updated successfully.</p>';
+        } else {
+            // Query failed
+            echo '<p>Refund status update failed.</p>';
+        }
+    }
+?>
