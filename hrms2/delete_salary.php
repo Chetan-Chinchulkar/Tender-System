6e95c9b7dd3ff69d5ca9d2076808e7fd21893726
Chetan Chinchulkar <76626599+Chetan-Chinchulkar@users.noreply.github.com>
2022-07-14 16:33:49 +0530

    Delete hrms2 directory

--- hrms2/delete_salary.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "include/connection.php";
-$id = $_GET["id"];
-mysqli_query($link, "delete from salay where id=$id") or die(mysqli_error($link));
-?>
-<script type="text/javascript">
-    window.location = "salary_payrool.php";
-</script>
--- hrms2/include/connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-$link = mysqli_connect("localhost", "root", "root", "test12345", 3325);
-mysqli_select_db($link, "hrms");
-?>
