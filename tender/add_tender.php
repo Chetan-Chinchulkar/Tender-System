21d82c2f47da9f1fab8d6563d9fd682816314a8c
Jwngfu Brahma <open.jwngfu@gmail.com>
2022-07-16 16:27:59 +0530

    Fixed the file upload issue
    Added enctype="multipart/form-data" in the form tag

--- tender/add_tender.php
+++ tender/add_tender.php
@@ -30,7 +30,7 @@ include 'include/navbar.php';
         <div class="main-wthree">
             <div class="container">
             <!-- create form  -->
-                <form action="add_tender.php" method="POST">
+                <form action="add_tender.php" method="POST" enctype="multipart/form-data">
                     <div class="form-group">
                         <label for="serial_number">Serial Number</label>
                         <input type="text" class="form-control" id="serial_number" name="serial_number" placeholder="Serial Number" disabled>
@@ -62,6 +62,7 @@ include 'include/navbar.php';
                     <!-- tender NIT upload -->
                     <div class="form-group">
                         <label for="tender_nit">Tender NIT</label>
+                        <label > .pdf, .txt, .jpeg format only</label>
                         <input type="file" class="form-control" id="tender_nit" name="tender_nit" placeholder="Tender NIT" >
                     </div>
 
@@ -92,6 +93,11 @@ include 'include/footer.php';
 
 ?>
 
+<form action="add_tender.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
 
 <!-- add php code to submit the form details -->
 <?php
@@ -99,41 +105,62 @@ include 'include/footer.php';
         // print $_SESSION['userid'] in alert
 
 
-        
-        if ($_SESSION['logged_in']==true) {
 
             // get data
             // $serial_number = $_POST['serial_number'];
-            $city = $_POST['city'];
-            $state = $_POST['state'];
-            $client_name = $_POST['client_name'];
-            $description = $_POST['description'];
-            $tender_number = $_POST['tender_number'];
-            $tender_date = $_POST['tender_date'];
-            $tender_nit = $_FILES['tender_nit']['name'];
-            $website_or_portal_link = $_POST['website_or_portal_link'];
-            $userid = $_SESSION['userid'];
+            $city = $_POST['city'] ?? "";
+            $state = $_POST['state'] ?? "";
+            $client_name = $_POST['client_name'] ?? "";
+            $description = $_POST['description'] ?? "";
+            $tender_number = $_POST['tender_number'] ?? "";
+            $tender_date = $_POST['tender_date'] ?? "";
+            // $tender_nit = $_POST['tender_nit'] ?? "";
+            // $tender_nit = $_FILES['tender_nit']['name'] ?? "";          
+            $website_or_portal_link = $_POST['website_or_portal_link'] ?? "";
+            $userid = $_SESSION['userid'] ?? "";
+
+            $tender_nit = $userid . "_" . basename($_FILES["tender_nit"]["name"]);
 
             // upload file
             $target_dir = "uploads/";
-            $target_file = $target_dir . basename($_FILES["tender_nit"]["name"]);
+            $target_file = $target_dir . $tender_nit;
             $uploadOk = 1;
 
             // Check if file already exists
-            if (file_exists($target_file)) {
-                echo "Sorry, file already exists.";
-                $uploadOk = 0;
-            }
+            // if (file_exists($target_file)) {
+            //     echo "Sorry, file already exists.";
+            //     $uploadOk = 0;
+            // }
             // Check file size
             if ($_FILES["tender_nit"]["size"] > 500000) {
                 echo "Sorry, your file is too large.";
                 $uploadOk = 0;
             }
 
-            // insert data into database
-            $sql = "INSERT INTO tender (city, state, client_name, description, tendernumber, tenderdate, tendernit, weblink, userid) VALUES ('$serial_number', '$city', '$state', '$client_name', '$description', '$tender_number', '$tender_date', '$tender_nit', '$website_or_portal_link', '$userid')";
 
+            // Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+                echo "Sorry, your file was not uploaded.";
+                exit;
+            // if everything is ok, try to upload file
+            } else {
+                if (move_uploaded_file($_FILES["tender_nit"]["tmp_name"], $target_file)) {
+                echo "The file ". htmlspecialchars( basename( $_FILES["tender_nit"]["name"])). " has been uploaded.";
+                } else {
+                echo "Sorry, there was an error uploading your file.";
+                }
+            }
+
+
+
+            // insert data into database
+            $sql = "INSERT INTO `tender_table` (`userid`,`serialno`, `city`, `states`, `clientname`, `descriptions`, `tendernumber`, `tenderdate`, `tendernit`, `weblink`) 
+                        VALUES ( '$userid',NULL, '$city', '$state', '$client_name', '$description', '$tender_number', '$tender_date', '$tender_nit' , '$website_or_portal_link')";
+
+            
             // $sql = "insert into tender_table (userid, city, state, clientname, description, tendernumber, tenderdate) values('$_SESSION[userid]', '$_POST[city]','$_POST[state]','$_POST[client_name]','$_POST[description]','$_POST[tender_number]','$_POST[tender_date]') ";
+            // $sql = "select * from admin where userid = '$userid'";
+            
             $res = mysqli_query($link, $sql) or die(mysqli_error($link));
 
             
@@ -143,16 +170,7 @@ include 'include/footer.php';
             </script>
             <?php
             // header("location: add_tender_bid.php");
-        }
-        else {
-            ?>
-            <script>
-                document.getElementById('failure').style.display = 'block';
-                // alert("Please login to continue");
-                // window.location.href = "login.php";
-            </script>
-            <?php
-        }
+      
     }
     
 ?>
